5a6cfa921fb6889154ad0379d4e697d338f5d03e
Niko Lockenvitz <nl@nikolockenvitz.de>
2024-03-09 16:47:11 +0100

    allow setting QR ecc and border size via console

--- qr-generator.js
+++ qr-generator.js
@@ -1,7 +1,7 @@
-const QR_CODE_DEFAULT_ECC         = qrcodegen.QrCode.Ecc.MEDIUM;
-const QR_CODE_DEFAULT_BORDER_SIZE = 4;
+window.QR_CODE_DEFAULT_ECC         = qrcodegen.QrCode.Ecc.MEDIUM;
+window.QR_CODE_DEFAULT_BORDER_SIZE = 4;
 
 export function getQrCodeSVG (content) {
-    let qrcode = qrcodegen.QrCode.encodeText(content, QR_CODE_DEFAULT_ECC);
-    return qrcode.toSvgString(QR_CODE_DEFAULT_BORDER_SIZE);
+    const qrcode = qrcodegen.QrCode.encodeText(content, window.QR_CODE_DEFAULT_ECC);
+    return qrcode.toSvgString(window.QR_CODE_DEFAULT_BORDER_SIZE);
 }
